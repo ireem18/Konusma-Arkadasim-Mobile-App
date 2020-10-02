4301d36b7d9cd9dfd1d499db7787fd4746c895ee
İrem Demiröz <40467163+ireem18@users.noreply.github.com>
2020-10-02 17:13:11 +0300

    Update serializers.py

--- konusma_arkadasim-django/konusma_arkadasim_api/uploadapp/serializers.py
+++ konusma_arkadasim-django/konusma_arkadasim_api/uploadapp/serializers.py
@@ -7,5 +7,4 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileAudio
         fields = "__all__"
-# bu serileştirmek diye türkçeleştirilmiş ama json veriyi yapıp çekmeye yarıyor
-# tam olarak işlevi json yapmak sanırım
+
